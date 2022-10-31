aad44dc9410c5bef020e01cd1ae50b7057a6cade
Ravil (ZuBy) Zaripov <rzaripov1990@gmail.com>
2022-10-31 20:03:43 +0600

    Create in.go

---
+++ in.go
@@ -0,0 +1,42 @@
+package go_tricks
+
+func in(val interface{}, args ...interface{}) (ok bool) {
+	switch t := val.(type) {
+	case string:
+		for _, v := range args {
+			ok = t == v
+			if ok {
+				return
+			}
+		}
+	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
+		for _, v := range args {
+			ok = t == v
+			if ok {
+				return
+			}
+		}
+	case float32, float64:
+		for _, v := range args {
+			ok = t == v
+			if ok {
+				return
+			}
+		}
+	case error:
+		for _, v := range args {
+			ok = errors.Is(t, v.(error))
+			if ok {
+				return
+			}
+		}
+	case bool:
+		for _, v := range args {
+			ok = t == v
+			if ok {
+				return
+			}
+		}
+	}
+	return
+}
